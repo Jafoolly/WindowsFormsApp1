75c65d0bfa66dd540cbf095670bdb17d6d73ebbe
George Clayton <G.Clayton8548@student.leedsbeckett.ac.uk>
2019-01-10 10:32:53 +0000

    Added Repeat circle and rectnagle

--- WindowsFormsApp1/ShapesInterface.cs
+++ WindowsFormsApp1/ShapesInterface.cs
@@ -4,6 +4,7 @@ namespace WindowsFormsApp1
 {
     interface ShapesInterface
     {
+        // setting rules for classes to call from.
         void set(Color c, params int[] list);
         void draw(Graphics g);
         double calcArea();
